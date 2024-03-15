cc6f41237b5a4a88b0aea0162dfd01385a45fee6
Axel Clark <axel.clark@pm.me>
2024-03-14 20:57:20 -0700

    Make update from remote

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -18,11 +18,6 @@ return {
           },
         },
       },
-      -- nnoremap / :Neotree toggle current reveal_force_cwd<cr>
-      -- nnoremap | :Neotree reveal<cr>
-      -- nnoremap gd :Neotree float reveal_file=<cfile> reveal_force_cwd<cr>
-      -- nnoremap <leader>b :Neotree toggle show buffers right<cr>
-      -- nnoremap <leader>s :Neotree float git_status<cr>
     }
   end,
 }
