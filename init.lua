6a58b809e69b8d02fd00f266bedcbb0de5c7c0e9
Axel Clark <axel.clark@pm.me>
2024-04-18 21:08:52 -0700

    Add friendly snippets and add html snippets to elixir

--- init.lua
+++ init.lua
@@ -739,12 +739,15 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              -- will exclude all elixir snippets since they are provided by lexical
+              require('luasnip.loaders.from_vscode').load {
+                exclude = { 'elixir' },
+              }
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
