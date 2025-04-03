37d004850909c49e9ff6397a8a085ab773667815
Axel Clark <axel@viabeacon.com>
2025-04-02 21:19:42 -0700

    Add chezmoi search

--- init.lua
+++ init.lua
@@ -421,6 +421,7 @@ require('lazy').setup({
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'GianniBYoung/chezmoi-telescope.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
@@ -467,6 +468,7 @@ require('lazy').setup({
       -- Enable telescope extensions, if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'chezmoi')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -481,6 +483,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', function() require('telescope').extensions.chezmoi.dotfiles() end, { desc = '[S]earch [C]hezmoi dotfiles' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
