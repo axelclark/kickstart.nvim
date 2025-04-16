dcfb854f178cc344581c83188cccd8a827de3a63
Axel Clark <axel@viabeacon.com>
2025-04-16 14:52:14 -0700

    Add dockerfile and terraform tools

--- init.lua
+++ init.lua
@@ -654,6 +654,9 @@ require('lazy').setup({
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
+        docker_compose_language_service = {},
+        dockerls = {},
+        terraformls = {},
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
@@ -736,6 +739,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
+        'markdownlint', -- Linter for Markdown
+        'hadolint', -- Linter for Dockerfiles
+        'tflint', -- Linter for Terraform
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -1020,19 +1026,24 @@ require('lazy').setup({
         'cmake',
         'cpp',
         'css',
+        'dockerfile',
         'eex',
         'elixir',
         'go',
         'heex',
         'html',
         'json',
+        'just',
         'lua',
         'markdown',
         'python',
         'rust',
+        'terraform',
+        'toml',
         'typescript',
         'vim',
         'vimdoc',
+        'yaml',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
