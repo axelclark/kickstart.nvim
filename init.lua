b7535787af27d8ad45ceaf7d4859db0e760aca33
Axel Clark <axel@viabeacon.com>
2025-06-03 13:20:33 -0700

    Add smart-splits.nvim

--- init.lua
+++ init.lua
@@ -208,14 +208,7 @@ vim.keymap.set('n', '<leader>tt', ':TestFile<CR>', { silent = true, desc = 'Run 
 vim.keymap.set('n', '<leader>ta', ':TestSuite<CR>', { silent = true, desc = 'Run test suite' })
 vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { silent = true, desc = 'Run last test' })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Window navigation handled by smart-splits plugin
 
 vim.keymap.set('n', 'bl', ':ls<CR>:b<Space>', { noremap = true, desc = 'list buffers for navigation' })
 
